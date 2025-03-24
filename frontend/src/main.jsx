607d2d86fcb5f9b57e3a78ce2f1f3bcdade1a0f6
DESKTOP-OTQA2NF\E-TIME <fsafdar627@gmail.com>
2025-03-24 20:49:35 +0500

    Implemented Redux Configuration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Provider } from "react-redux";
+import { store } from './redux/store/store.js'
 import App from './App.jsx'
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
@@ -11,7 +13,10 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-    <App />
+    <Provider store={store}>
+        <App /> 
+
+      </Provider>
     <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
