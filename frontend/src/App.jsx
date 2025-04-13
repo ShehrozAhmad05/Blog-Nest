72a2213ab1de90bae1afd9a7c96e25a52114d71e
Fatima-safdar91 <fsafdar627@gmail.com>
2025-04-13 23:50:33 +0500

    Added Pricing Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,6 +19,7 @@ import UserDashboard from './components/User/UserDashboard'
 import AccountSummaryDashboard from './components/User/AccountSummary'
 import AddCategory from './components/Category/AddCategory'
 import CreatePlan from './components/Plans/CreatePlan'
+import Pricing from './components/Plans/Pricing'
 
 function App() {
 
@@ -63,10 +64,12 @@ function App() {
             <AddCategory />
           </AuthRoute>} path='add-category' />
         </Route>
+        {/* Public Links */}
         <Route element={<PostsList />} path='/posts' />
         {/* <Route element={<UpdatePost />} path='/posts/:postId'/> */}
         <Route element={<PostDetails />} path='/posts/:postId' />
         <Route element={<Register />} path='/register' />
+        <Route element={<Pricing />} path='/pricing' />
         <Route element={<AuthRoute>
           <Profile />
         </AuthRoute>} path='/profile' />
